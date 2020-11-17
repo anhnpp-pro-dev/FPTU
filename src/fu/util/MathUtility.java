f54377cac1453d15f7f03ade4e165992b18d6fe0
anhnpp.pro.dev <nguyenphanphuocanh121@gmail.com>
2020-11-17 08:40:10 +0700

    moi upload len lan dau

---
+++ src/fu/util/MathUtility.java
@@ -0,0 +1,19 @@
+package fu.util;
+
+/**
+ *
+ * @author KING (Nguyễn Phan Phước Anh)
+ */
+public class MathUtility {
+
+    //Class này chứa các hàm tiện ích dùng chung cho mọ nơi 
+    //do đó  nó phải nên là static
+    public static final double PI = 3.1415;
+
+    //hàm tính n giai thừa, n >= 0 && n <= 20
+    //nếu quá 20 tràn kiểu long
+    //0! = 1; 1! = 1; n!= 1.2.3...n
+    public static long getFactorial(int n) {
+
+    }
+}
